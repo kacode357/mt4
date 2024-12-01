a2c6dc4a31d9eae9bac6cff147382243a983d275
Ka Ka Lưu <kacode357@gmail.com>
2024-12-01 09:42:23 +0700

    create project v1.0

--- src/components/homepage/ImageList.tsx
+++ src/components/homepage/ImageList.tsx
@@ -1,33 +1,40 @@
 import { Card, Row, Col } from "antd";
 import vanmieu from "../../assets/vanmieu1.png";
+import chuaMotCot from "../../assets/chua-mot-cot.png";
+import langBac from "../../assets/lang-bac.png";
+import hoHoanKiem from "../../assets/ho-hoan-kiem.png";
+import cauLongBien from "../../assets/cau-long-bien.png";
+import denNgocSon from "../../assets/den-ngoc-son.png";
 
 const ImageList = () => {
   const images = [
-    { id: 1, img: vanmieu, title: "Văn Miếu ", author: "Tác giả A" },
-    { id: 2, img: vanmieu, title: "Chùa Một Cột", author: "Tác giả B" },
-    { id: 3, img: vanmieu, title: "Lăng Bác", author: "Tác giả C" },
-    { id: 4, img: vanmieu, title: "Hồ Hoàn Kiếm", author: "Tác giả D" },
-    { id: 5, img: vanmieu, title: "Cầu Long Biên", author: "Tác giả E" },
-    { id: 6, img: vanmieu, title: "Đền Ngọc Sơn", author: "Tác giả F" },
-    { id: 7, img: vanmieu, title: "Văn Miếu 2", author: "Tác giả G" },
-    { id: 8, img: vanmieu, title: "Chùa Một Cột 2", author: "Tác giả H" },
+    { id: 1, img: vanmieu, title: "Văn Miếu", author: "Tác giả A" },
+    { id: 2, img: chuaMotCot, title: "Chùa Một Cột", author: "Tác giả B" },
+    { id: 3, img: langBac, title: "Lăng Bác", author: "Tác giả C" },
+    { id: 4, img: hoHoanKiem, title: "Hồ Hoàn Kiếm", author: "Tác giả D" },
+    { id: 5, img: cauLongBien, title: "Cầu Long Biên", author: "Tác giả E" },
+    { id: 6, img: denNgocSon, title: "Đền Ngọc Sơn", author: "Tác giả F" },
   ];
 
   return (
     <div className="container bg-[#fdf4e3] py-2 px-4">
-      {/* Row để chứa các thẻ Card */}
-      <Row gutter={[16, 24]} justify="center">
-        {/* Hàng trên: 4 ảnh */}
-        {images.slice(0, 4).map((image) => (
-          <Col span={6} key={image.id} className="flex justify-center">
+      {/* Tiêu đề cho component */}
+      <h2 className="text-4xl font-semibold text-center mb-8 text-transparent bg-clip-text bg-gradient-to-r from-[#181818] to-[#353539b0] drop-shadow-lg">
+        Danh Sách Ảnh Các Di Tích Lịch Sử
+      </h2>
+      
+      {/* Row để chứa các thẻ Card cho 3 ảnh trên */}
+      <Row gutter={[8, 12]} justify="center">
+        {images.slice(0, 3).map((image) => (
+          <Col span={8} key={image.id} className="flex justify-center">
             <Card
               hoverable
               cover={
-                <div className="overflow-hidden rounded-lg">
+                <div className="overflow-hidden rounded-lg h-64">
                   <img
                     alt={image.title}
                     src={image.img}
-                    className="transform scale-120 w-full transition-transform duration-300"
+                    className="object-cover w-full h-full transition-transform duration-300"
                   />
                 </div>
               }
@@ -42,19 +49,18 @@ const ImageList = () => {
         ))}
       </Row>
 
-      {/* Tăng khoảng cách giữa hai hàng */}
-      <Row gutter={[16, 24]} justify="center" style={{ marginTop: '12px' }}>
-        {/* Hàng dưới: 4 ảnh */}
-        {images.slice(4, 8).map((image) => (
-          <Col span={6} key={image.id} className="flex justify-center">
+      {/* Row để chứa các thẻ Card cho 3 ảnh dưới */}
+      <Row gutter={[8, 12]} justify="center" style={{ marginTop: '12px' }}>
+        {images.slice(3, 6).map((image) => (
+          <Col span={8} key={image.id} className="flex justify-center">
             <Card
               hoverable
               cover={
-                <div className="overflow-hidden rounded-lg">
+                <div className="overflow-hidden rounded-lg h-64">
                   <img
                     alt={image.title}
                     src={image.img}
-                    className="transform scale-120 w-full transition-transform duration-300"
+                    className="object-cover w-full h-full transition-transform duration-300"
                   />
                 </div>
               }
